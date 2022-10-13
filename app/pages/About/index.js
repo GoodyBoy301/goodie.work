a1a9ff01084c5f942293e401f461687188740b73
Goodness <goodyboy301@gmail.com>
2022-10-13 21:20:10 +0100

    feat: create About

--- app/pages/About/index.js
+++ app/pages/About/index.js
@@ -1,7 +1,33 @@
 import Page from "classes/Page";
+import gsap from "gsap";
+import { split } from "utils/text";
+import aboutIntro from "animations/aboutIntro";
 
 export default class About extends Page {
   constructor() {
-    super({ element: ".about", id: "about" });
+    super({
+      element: ".about",
+      id: "about",
+      elements: {
+        texts: ".about__text",
+        images: ".about__media__image",
+      },
+    });
+  }
+
+  /** Life Cycle */
+  create() {
+    super.create();
+    this.reCalculate();
+    this.elements.texts.forEach((element) => {
+      split({ element });
+    });
+    this.elements.spans = document.querySelectorAll(".about__text span");
+    const introAnimatiom = aboutIntro(this.elements);
+    introAnimatiom.play();
+  }
+
+  reCalculate() {
+    this.isMobile = innerWidth < 768;
   }
 }
