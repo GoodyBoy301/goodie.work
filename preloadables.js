4cde1b6956e302a1d25cbefd351df2f8f719059b
Goodness <goodyboy301@gmail.com>
2022-10-30 21:43:17 +0100

    Initial commit

---
+++ preloadables.js
@@ -0,0 +1,32 @@
+const preloadables = {};
+preloadables.images = [
+  "/images/baked.jpg",
+  "/images/endl_1.png",
+  "/images/endl_2.png",
+  "/images/endl_3.png",
+  "/images/flag.jpg",
+  "/images/goody.png",
+  "/images/howdy_1.png",
+  "/images/howdy_2.png",
+  "/images/howdy_3.png",
+  "/images/images.png",
+  "/images/untitled01_1.png",
+  "/images/untitled01_2.png",
+  "/images/untitled01_3.png",
+  "/images/untitled02_1.png",
+  "/images/untitled02_2.png",
+  "/images/untitled02_3.png",
+  "/images/vsl_1.png",
+  "/images/vsl_2.png",
+  "/images/vsl_3.png",
+];
+preloadables.textures = {
+  home: "/images/baked.jpg",
+  about: "/images/flag.jpg",
+};
+
+preloadables.exports = {
+  home: "scene.glb",
+};
+
+module.exports = preloadables;
