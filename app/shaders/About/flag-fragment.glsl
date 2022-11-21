a61d9ae967013b64f3b6800153424eee145cabd1
Goodness <goodyboy301@gmail.com>
2022-11-21 22:32:09 +0100

    feat: animate FragColor

--- app/shaders/About/flag-fragment.glsl
+++ app/shaders/About/flag-fragment.glsl
@@ -1,6 +1,8 @@
 precision highp float;
 
 uniform sampler2D uTexture;
+uniform float uOpacity;
+uniform float uShade;
 
 varying vec2 vUv;
 
@@ -8,5 +10,5 @@ void main(){
   vec2 uv = vUv;
 
   vec4 texture = texture2D(uTexture, uv);
-  gl_FragColor = texture;
+  gl_FragColor = vec4(texture.xyz * uShade, uOpacity);
 }
